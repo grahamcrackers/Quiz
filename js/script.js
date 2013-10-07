913bbfe79ec86a9f061e82abe8288589e03988f1
grahamcrackers <grogers12487@gmail.com>
2013-10-07 16:50:54 -0500

    fading works

--- js/script.js
+++ js/script.js
@@ -15,9 +15,9 @@ function getAllQuestions(values) {
 //function to reuse the showQuestion logic to use the back button
 function getQuestion() {
     for(var i=0; i < allQuestions.length; i++){
+
             if(qCount === i){
-                //$('#Question').html(allQuestions[i].question).fadeIn();
-                currentQuestion = allQuestions[i].question;
+                $('#Question').html("<h1>"+allQuestions[i].question+"</h1>").fadeIn();
                 //console.log("Answer to the question is :" + allQuestions[i].correctAnswer + " - Score:"+score);
 
                 //displays list of choices for current object in the array
@@ -25,25 +25,24 @@ function getQuestion() {
                     $('#Selection').append("<br><label><input id='choices' type='radio' name='choice' value='"+j+"'>"+answers[j]+"</input></label>");
                 }
 
+                currentChoices = allQuestions[i].choices;
+
                 cAns = allQuestions[i].correctAnswer;
             } 
         }
 }
 
-function showQuestion() {
 
-}
 
 //users score and position in array
 var score = 0;	
 var qCount;
 var cAns;
 var selectedAnswer;
-var userAnswers = [];
 var allQuestions = [];
 var currentQuestion;
-var currentChoices;
-
+var currentChoices = [];
+var userAnswers = [];
 
 //grab each question from JSON data and put it into global array allQuestions
 $.getJSON('data/questions.json', function(json){
@@ -56,73 +55,87 @@ $.getJSON('data/questions.json', function(json){
     });  
 });
 
-$('#Question').fadeIn();
+
 
     //On click, loop through array of objects to grab the question
-    $('#Next').on('click', function(){
+    $('.container').on('click', '#Next', function(){
 
-        
-        //checks user input
-        selectedAnswer = $("input[name='choice']:checked").val();
-        console.log("Selected Answer: " + selectedAnswer);
+        $('#Question').fadeOut( function (){
 
-
-        //validation logic could be implemented like this
-        if(selectedAnswer == null && $('#Selection').html().length > 0)
-        {
-            alert("Dude... pick an answer.");
-            return false;
-        } if ($('#Selection').html().length === 0) {
-            qCount = 0
-        }else {
-            //Question count
-            qCount++;
-        }
+            //checks user input
+            selectedAnswer = $("input[name='choice']:checked").val();
+            //console.log("Selected Answer: " + selectedAnswer);
 
 
-        //if selected answer is equal to correct answer, add 1
-        console.log("Now comparing your answer: " + selectedAnswer  + " to correct answer: " + cAns);
-        if(selectedAnswer && selectedAnswer == cAns ){
-            //score will not update until next question with this logic
-            score++;
-        }
+            //validation logic could be implemented like this
+            if(selectedAnswer == null && $('#Selection').html().length > 0)
+            {
+                alert("Dude... pick an answer.");
+                return false;
+            } else if ($('#Selection').html().length === 0) {
+                qCount = 0
+            } else {
+                
+                $('#Question').fadeIn();
+                userAnswers.push(selectedAnswer);
+                qCount++;
+            }
 
+            if(userAnswers != undefined){
+            console.log("User Answers: "+userAnswers.join(""));
+            };
 
-        //removes answers before next question is loaded
-        $('#Selection').children().remove();
+            //if selected answer is equal to correct answer, add 1
+            //console.log("Now comparing your answer: " + selectedAnswer  + " to correct answer: " + cAns);
+            if(selectedAnswer && selectedAnswer == cAns ){
+                //score will not update until next question with this logic
+                score++;
+            } 
 
-        //displays question of current object in the array
-        getQuestion();
+            //removes answers before next question is loaded
+            $('#Selection').children().remove();
 
-        
+            //displays question of current object in the array
+            getQuestion();
 
-        if(qCount === 2){
-            $('#Selection').after("<button id='Back' class='btn btn-success'> Back </button>")
-        }else if (qCount > 10){
-            $('#Question').html("Your final score is " + score+"/10");
-            $('#Next').remove();
-            $('#Back').remove();
-        }
-        console.log("Question Count: "+qCount);
+            
 
+            if(qCount === 1){
+                $('#Selection').after("<button id='Back' class='btn btn-success'> Back </button>");
+            }else if (qCount > allQuestions.length){
+                $('#Question').html("Your final score is: "+score+"/"+allQuestions.length);
+                $('#Next').remove();
+                $('#Back').remove();
+            }
+
+            console.log("Question Count: "+qCount);
+
+        });
 
     });  //End $('Next').click
 
+    
     $('.container').on('click', '#Back', function(){
         
-        qCount--;
-        $('#Selection').children().remove();
-        getQuestion();
-        console.log("Question Count: "+qCount);
+        $('#Question').fadeOut(function(){
+            
+            //reduce question count
+            qCount--;
+            
+            $('#Selection').children().remove();
+            
+            getQuestion();
+            
+            console.log("Question Count: "+qCount);
 
-        if (qCount < 2){
-            $('#Back').remove();
-        }
+            if (qCount < 1){
+                $('#Back').remove();
+            }
+
+            $('#Question').fadeIn();
+
+        });
     
     }); //End $('#Back').click
-
-    $('.container').on('click', '#Fade', function(){
-        
-        $('#Question').fadeToggle();
-    }); //End $('#Back').click
+    
 }); //End Document
