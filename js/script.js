c8d5070d42d76866f95ed2fa34e141f39c6d78b0
grahamcrackers <grogers12487@gmail.com>
2013-10-10 16:29:41 -0500

    storing user answers works

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 "use strict";
 
+//function object
 function Question (theQuestion, theChoices, theCorrectAnswer){
 	this.question = theQuestion;
 	this.choices = theChoices;
@@ -12,7 +13,7 @@ function getAllQuestions(values) {
         allQuestions.push(values);
 }
 
-//function to reuse the showQuestion logic to use the back button
+//function to reuse the showQuestion logic to use the with back button
 function getQuestion() {
     for(var i=0; i < allQuestions.length; i++){
 
@@ -32,13 +33,35 @@ function getQuestion() {
         }
 }
 
+function checkRadio() {
+    for (var i=0; i<userAnswers.length; i++) {
+        if (qCount === i){
+            $('input[value='+userAnswers[i]+']').prop('checked',true);
+        }
+    } 
+}
 
 function showQuestion() {
     //removes answers before next question is loaded
     $('#Selection').children().remove();
     getQuestion();
+    showButtons();
+    checkRadio();
 }
 
+function showButtons() {
+    //will insert a back button after the first question
+    if(qCount > 0 && $('#Back').length == 0){
+        $('#Selection').after("<button id='Back' class='btn btn-success'> Back </button>");
+        //$('#Next').before("<button id='Back' class='btn btn-success'> Back </button>");
+    } else if (qCount < 1){
+        $('#Back').remove();
+    } else if (qCount >= allQuestions.length){
+        $('#Question').html("<h1>Your final score is: "+score+"/"+allQuestions.length+"</h1>");
+        $('#Next').remove();
+        $('#Back').remove();
+    }
+}
 
 //users score and position in array
 var score = 0;	
@@ -72,20 +95,23 @@ $.getJSON('data/questions.json', function(json){
 
         $('#Question').fadeOut( function (){
 
+            //fade in question
             $('#Question').fadeIn();
 
-            //validation logic could be implemented like this
+            //validation logic
             if(selectedAnswer == null && $('#Selection').html().length > 0)
             {
                 alert("Dude... pick an answer.");
                 return false;
             } if ($('#Selection').html().length === 0) {
+                //will start quiz count when next button is hit the first time
                 qCount = 0
             }else {
-
+                //stores user answer in array
                 userAnswers[qCount] = selectedAnswer;
                 qCount++;
             }
+
 
             if(userAnswers != null){
             console.log("User Answers: "+userAnswers.join());
@@ -93,22 +119,13 @@ $.getJSON('data/questions.json', function(json){
 
             //if selected answer is equal to correct answer, add 1
             //console.log("Now comparing your answer: " + selectedAnswer  + " to correct answer: " + cAns);
-            if(selectedAnswer && selectedAnswer == cAns ){
+            if(isNaN(userAnswers[qCount]) && selectedAnswer == cAns ){
                 //score will not update until next question with this logic
                 score++;
-            } 
+            } //else if ()
 
             //displays question of current object in the array
             showQuestion();
- 
-            //will insert a back button after the first question
-            if(qCount === 1){
-                $('#Selection').after("<button id='Back' class='btn btn-success'> Back </button>");
-            }else if (qCount >= allQuestions.length){
-                $('#Question').html("<h1>Your final score is: "+score+"/"+allQuestions.length+"</h1>");
-                $('#Next').remove();
-                $('#Back').remove();
-            }
 
             console.log("Question Count: "+qCount);
 
@@ -127,30 +144,8 @@ $.getJSON('data/questions.json', function(json){
             qCount--;
 
             showQuestion();
-            
-            
-            //for (var i=0; i<userAnswers.length; i++){
-                //if (qCount === i){
-                    $.each($('.choice'),function(){
-                    console.log(this.val());
-                });
-                    //if ($("input[name='choice']").val() === userAnswers[i]){
-                            //alert($("input[name='choice']").val());
-                            //$('choice').attr('checked', true);
-                        //}
-                    
-                   // }
-                //}
-            
-            
 
             console.log("Question Count: "+qCount);
-
-            if (qCount < 1){
-                $('#Back').remove();
-            }
-
-            
 
         });
     
